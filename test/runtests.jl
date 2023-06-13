bd3f802c2e950752e9913301073f62caf9947d79
Emiko Soroka <emiko@me.com>
2023-06-13 10:30:20 -0700

    The previous thing doesn't work because the container doesn't have z3

--- test/runtests.jl
+++ test/runtests.jl
@@ -164,6 +164,7 @@ end
     @test value(and(prob.children[1], prob.children[2])) == 1
 end
 
+#=
 @testset "Solving an SMT problem" begin
     x = Bool(3, "x")
     y = Bool(2, "y")
@@ -181,41 +182,4 @@ end
     @test all(value(y) .== [0 0])
 
 end
-
-# TODO 4/26/23
-# Fix names (just add them for now) - done 4/28/23
-# Fix equality comparison - done 4/28/23
-# Start SMTLIB2 section - done 4/30/23
-
-# TODO 5/1/23
-# Fix broadcasting ambiguity - done 5/6/23
-# Finish unit tests for operators  - done 5/6/23
-# Add docstrings to all functions
-# Add return types to all functions - resolved, not necessary. 5/6/23
-# Clean up export list
-
-# TODO 5/6/23
-# Change to arrays of 1-valued BoolExpr - done 5/6/23
-# Implement all(z1 .∧ z2) and any(z1 .∨ z2) behavior
-# Fix SMT statement functions after change - done
-
-# TODO 5/12/23
-# Fix naming to be more coherent, for example using hashing to generate unique hex names for combined operators - done 5/13/23
-# Fix unittests to match new naming scheme - done
-
-# TODO 5/16/23
-# Write a function that saves the problem to a file, ending with (check-sat) - done
-# Write a function that opens an smt2 input terminal to z3 and inputs the problem, then issues (check-sat) if no errors occur - modified, we made a function that calls z3 on the file
-# Write a function that retrieves (parses) the solution from z3 - done
-# Write larger demo with scheduling problem
-
-# TODO 5/25/23
-# Fix bugs with any() and all() - done
-# Write function that propagates values from :Identity elements to logical statements - done
-# Fix constructor to use values if they are present, eg if x.value = true then not(x).value = false - done
-# Add support for literals
-# Fix horrible bug with negation - done 6/3/23
-# Fix so 1x1 expressions are single BoolExprs instead of 1x1 matrix - possibly done?
-# Look into defining getproperty(x::Array{BoolExpr}, f::Symbol) to fix the non-working-ness of x.value
-# Fix calling sat!(Array{BoolExpr}) so it works. - done 6/2/23
-# Fix export to not export helper functions and use BooleanSatisfiability. to access them for unittests
+=#
