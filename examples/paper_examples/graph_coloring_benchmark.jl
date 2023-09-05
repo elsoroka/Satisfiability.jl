0fdf3926ddd67b2fb995aafff02c726f5a3fa100
Emiko Soroka <emiko@me.com>
2023-09-05 14:30:42 -0700

    Graph coloring benchmark, ready to run.

--- examples/paper_examples/graph_coloring_benchmark.jl
+++ examples/paper_examples/graph_coloring_benchmark.jl
@@ -1,39 +1,46 @@
 # NOTE THAT THIS FILE IS SET UP TO BE RUN FROM examples/paper_examples
 push!(LOAD_PATH, "../../src/")
 using Satisfiability, BenchmarkTools
+using StatsBase, Random
+Random.seed!(97)
 
 
 # A classic problem in graph theory is figuring out how to color nodes of a graph such that no two adjacent nodes have the same color.
 # In this example we time a graph coloring task using interactive solving.
-# The goal is to find a certain number of solutions for a prism graph (https://mathworld.wolfram.com/PrismGraph.html) of size n.
+# The goal is to find up to a fixed number of colorings for a randomly generated graph of size n with n links
 
-function graph_coloring(n::Int, n_to_find=3, limit=4, solutions=Array)
-    @satvariable(nodes[1:2*n], Int)
-    inner = nodes[1:n]
-    outer = nodes[n+1:end]
+# returns a list of nodes and a list of edges as tuples
+function make_graph(n::Integer, n_edges=div(n*3, 2))
+    A = collect(Iterators.product(1:n, 1:n))
+    pairs = cat([A[i, i+1:n] for i=1:n]..., dims=1)
+    edges = sample(pairs, Weights(ones(length(pairs))), min(length(pairs), n_edges), replace=false)
+    return 1:n, edges
+end
 
+function graph_coloring(n::Int, edges, n_to_find=3, limit=3, solutions=Array)
+    @satvariable(nodes[1:n], Int)
     limits = and.(nodes .>= 1, nodes .<= limit)
 
-    connections = cat([distinct(inner[i], inner[i+1]) for i=1:n-1],
-                      [distinct(outer[i], outer[i+1]) for i=1:n-1],
-                      [distinct(inner[end], inner[1]), distinct(outer[end], outer[1])],
-                      [distinct(inner[i], outer[i]) for i=1:n], dims=1)
+    connections = cat([distinct(nodes[i], nodes[j]) for (i,j) in edges], dims=1)
 
     open(Z3()) do interactive_solver
         # make assertion # add "(set-logic QF_LIA)\n"
+        send_command(interactive_solver, "(set-logic QF_LIA)", dont_wait=true)
         assert!(interactive_solver, limits, connections)
         i = 1
         while i <= n_to_find
             # Try to solve the problem
             status, assignment = sat!(interactive_solver)
-            if status != :SAT
-                error("Something went wrong!")
+            if status == :SAT
+                assign!(nodes, assignment)
+                solutions[i,:] .= value(nodes)
+                #println("i = $i, status = $status, assignment = $assignment")
+                # Use assert! to exclude the solution we just found.
+                assert!(interactive_solver, not(and(nodes .== value(nodes))))
+            else
+                #println("unsat!")
+                break
             end
-            assign!(nodes, assignment)
-            solutions[i,:] .= value(nodes)
-            #println("i = $i, status = $status, assignment = $assignment")
-            # Use assert! to exclude the solution we just found.
-            assert!(interactive_solver, not(and(nodes .== value(nodes))))
             i += 1
         end
     end
@@ -41,19 +48,11 @@ function graph_coloring(n::Int, n_to_find=3, limit=4, solutions=Array)
 end
 
 # solutions is a list of list of values, where each one is progressively excluded
-function make_smt_file(n::Int, solutions::Array, limit=4)
-
-    # We construct this of size n https://en.wikipedia.org/wiki/Prism_graph
-    @satvariable(nodes[1:2*n], Int)
-    inner = nodes[1:n]
-    outer = nodes[n+1:end]
-
+function make_smt_file(n::Int, edges, limit=4, solutions=Array)
+    @satvariable(nodes[1:n], Int)
     limits = and.(nodes .>= 1, nodes .<= limit)
 
-    connections = cat([distinct(inner[i], inner[i+1]) for i=1:n-1],
-                      [distinct(outer[i], outer[i+1]) for i=1:n-1],
-                      [distinct(inner[end], inner[1]), distinct(outer[end], outer[1])],
-                      [distinct(inner[i], outer[i]) for i=1:n], dims=1)
+    connections = cat([distinct(nodes[i], nodes[j]) for (i,j) in edges], dims=1)
     
     outfile = open("graph_genfiles/graph_coloring_gen_$n.smt", "w+")
     write(outfile,"(set-logic QF_LIA)\n")
@@ -61,9 +60,11 @@ function make_smt_file(n::Int, solutions::Array, limit=4)
     write(outfile, "(check-sat)\n(get-model)\n")
 
     for solution in eachrow(solutions)
-        #println(solution)
-        write(outfile, smt(not(and(nodes .== solution)), assert=true, as_list=true)[end])
-        write(outfile, "\n(check-sat)\n(get-model)\n")
+        if !all(solution .== 0) # if they're 0 no solution is in this row
+            #println(solution)
+            write(outfile, smt(not(and(nodes .== solution)), assert=true, as_list=true)[end])
+            write(outfile, "\n(check-sat)\n(get-model)\n")
+        end
     end
     close(outfile)
 end
@@ -79,14 +80,15 @@ end
 
 # Timing
 
-to_find=3 # find 3 solutions
+to_find=5 # find up to this many solutions
 
 # Cause precompilation
-s = zeros(to_find, 3*2)
-graph_coloring(3, to_find, 4, s)
-make_smt_file(3, s, 4)
-result = run_with_timing!(`timeout 20m z3 -smt2 graph_genfiles/graph_coloring_gen_2.smt`)
-println("got $s")
+s = zeros(to_find, 4)
+_,edges = make_graph(4)
+graph_coloring(4, edges, to_find, 1, s)
+make_smt_file(4, edges, 1, s)
+result = run_with_timing!(`timeout 20m z3 -smt2 graph_genfiles/graph_coloring_gen_4.smt`)
+println("got $s in precompilation")
 
 open("graph_execution_log_$(time()).txt", "w") do graph_execution_log
 
@@ -100,24 +102,33 @@ open("graph_execution_log_$(time()).txt", "w") do graph_execution_log
     fill!(satjl_timing, missing)
     z3_timing = Array{Union{Missing, Float64}}(undef, 20)
     fill!(satjl_timing, missing)
+    filegen_timing = Array{Union{Missing, Float64}}(undef, 20)
+    fill!(filegen_timing, missing)
 
-    write(graph_execution_log, "n,sat_timing (seconds),z3_timing (seconds)\n")
+    write(graph_execution_log, "n,sat_timing (seconds),z3_timing (seconds),filegen (seconds)\n")
+    println("n,sat_timing (seconds),z3_timing (seconds),filegen (seconds)\n")
     
+    samples = [10; 10; 10; 10; 5; 5; 5; ones(Int, 12)]
+    for i=4:16
+        n = 2^i
+        solutions=zeros(Int, to_find, n)
 
-    for i=3:20
-        solutions=zeros(Int, to_find, 2*i)
-        b1 = @benchmarkable graph_coloring($i, $to_find, 4, $solutions) samples=10
+        n_edges = i <= 6 ? div(3*n, 2) : 2*n # number of edges should ensure there are solutions, otherwise it's not quite a fair comparison
+        _, edges = make_graph(n, n_edges)
+        b1 = @benchmarkable graph_coloring($n, $edges, $to_find, 3, $solutions) samples=samples[i]
         t = run(b1)
         satjl_timing[i] = mean(t).time*1e-9 # the 1e-9 converts the time to seconds
         
-        make_smt_file(i, solutions, 4)
-
-        cmd = `timeout 20m z3 -smt2 graph_genfiles/graph_coloring_gen_$i.smt`
-        b2 = @benchmarkable run_with_timing!($cmd) samples=10
+        b2 = @benchmarkable make_smt_file(n, edges, 3, solutions) samples=1
         t = run(b2)
+        filegen_timing[i] = mean(t).time*1e-9 
+
+        cmd = `timeout 20m z3 -smt2 graph_genfiles/graph_coloring_gen_$n.smt`
+        b3 = @benchmarkable run_with_timing!($cmd) samples=samples[i]
+        t = run(b3)
         z3_timing[i] = mean(t).time*1e-9 
 
-        write(graph_execution_log, "$i,$(satjl_timing[i]),$(z3_timing[i])\n" )
-        println("$i $(satjl_timing[i]) $(z3_timing[i])")
+        write(graph_execution_log, "$n,$(satjl_timing[i]),$(z3_timing[i]),$(filegen_timing[i])\n" )
+        println("$n $(satjl_timing[i]) $(z3_timing[i]) $(filegen_timing[i])")
     end
 end
