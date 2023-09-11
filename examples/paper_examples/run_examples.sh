7db3cea24816b097f8a51d9e101a812fc2aad4f4
Emiko Soroka <emiko@me.com>
2023-09-11 14:37:38 -0700

    moved out of place file

--- examples/paper_examples/run_examples.sh
+++ examples/paper_examples/run_examples.sh
@@ -9,3 +9,10 @@ julia graph_coloring_benchmark.jl
 echo "File generation time plot"
 julia plot_file_generation_time.jl
 
+mkdir results
+mv graph*.txt results/
+mv pigeons*.txt results/
+mv *.pdf results/
+cp -r graph_genfiles results/graph_genfiles
+cp -r pigeons_genfiles results/pigeons_genfiles
+echo "Done, please run `docker cp` to retrieve the results."
