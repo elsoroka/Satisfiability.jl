9ff9d9695e22e61afe4b7f9530d0cf08f4ca148f
Fe-r-oz <feroz.ahmad.email@gmail.com>
2024-12-16 16:59:31 +0500

    improve unit testing via TestItemRunner

--- test/README_tests.jl
+++ test/README_tests.jl
@@ -1,30 +1,30 @@
-using Satisfiability
-using Test
+#@testitem "README.md exampes" begin
 
-function test_julia_examples_in_markdown(path)
-  file = read(path, String)
-  file_by_code_blocks = split(file, "```julia")[2:end]
-  possible_examples = map(s -> first(split(s, "```")), file_by_code_blocks)
-  for example ∈ possible_examples
-    quote_example = "begin\n" * example * "\nend"
-    parsed =
-      @test_nowarn Meta.parse(quote_example)
-    @test try
-      eval(parsed)
-      true
-    catch e
-      showerror(stderr, e, catch_backtrace())
-      println()
-      false
+    using Satisfiability
+
+    function test_julia_examples_in_markdown(path)
+        file = read(path, String)
+        file_by_code_blocks = split(file, "```julia")[2:end]
+        possible_examples = map(s -> first(split(s, "```")), file_by_code_blocks)
+        for example ∈ possible_examples
+            quote_example = "begin\n" * example * "\nend"
+            parsed = @test_nowarn Meta.parse(quote_example)
+            @test try
+                eval(parsed)
+                true
+            catch e
+                showerror(stderr, e, catch_backtrace())
+                println()
+                false
+            end
+        end
     end
-  end
-end
 
-@testset "Test README.md examples" begin
-  using Satisfiability
-  # Suppress printouts since this only tests that the examples in the README run.
-  std_ = stdout
-  redirect_stdout(devnull)
-  test_julia_examples_in_markdown("../README.md")
-  redirect_stdout(std_)
-end
+    @testset "Test README.md examples" begin
+        # Suppress printouts since this only tests that the examples in the README run.
+        std_ = stdout
+        redirect_stdout(devnull)
+        test_julia_examples_in_markdown("../README.md")
+        redirect_stdout(std_)
+    end
+#end
